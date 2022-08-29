992c7d2fee644d8d69fa42e2f5b188b721894d90
Gabriel-Quadros <gquadros312@gmail.com>
2022-08-29 08:44:59 -0300

    Aula 7

--- garagem/urls.py
+++ garagem/urls.py
@@ -3,10 +3,12 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from core.views import CategoriaViewSet
+from core.views import CategoriaViewSet, MarcaViewSet
 
 router = DefaultRouter()
 router.register(r'categorias', CategoriaViewSet)
+router.register(r'marca', MarcaViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
